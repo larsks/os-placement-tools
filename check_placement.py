f2552ad868e067775fbf48e97b37bb0915804b84
Lars Kellogg-Stedman <lars@oddbit.com>
2018-05-01 11:18:25 -0400

    start work on adding argparse/os_client_config support

--- check_placement.py
+++ check_placement.py
@@ -2,71 +2,102 @@
 
 from __future__ import print_function
 
-import json
+import argparse
+import logging
+import os_client_config
 import shade
+import sys
+
+LOG = logging.getLogger(__name__)
+cloud_config = os_client_config.OpenStackConfig()
 
 
 class Placement(object):
-    def __init__(self, api_url, session):
-        self.api_url = api_url
-        self.session = session
+    def __init__(self, cloud):
+        self.cloud = cloud
+        self.session = cloud.keystone_session
+
+        self.lookup_endpoint()
+
+    def lookup_endpoint(self):
+        placement_svc = self.cloud.get_service('placement')
+        placement_endpoint = self.cloud.search_endpoints(
+            filters=dict(service_id=placement_svc.id, interface='public'))[0]
+
+        placement_url = placement_endpoint['url']
+        LOG.info('using placement api @ {}'.format(placement_url))
+        self.placement_url = placement_url
 
     def list_resource_providers(self):
         res = self.session.get(
-            self.api_url + '/resource_providers')
+            self.placement_url + '/resource_providers')
         return res.json().get('resource_providers', [])
 
     def get_resource_provider(self, uuid):
         res = self.session.get(
-            self.api_url + '/resource_providers/{}'.format(uuid))
+            self.placement_url + '/resource_providers/{}'.format(uuid))
         return res.json()
 
     def get_resource_provider_allocations(self, uuid):
         res = self.session.get(
-            self.api_url + '/resource_providers/{}/allocations'.format(uuid))
+            self.placement_url + '/resource_providers/{}/allocations'.format(uuid))
         return res.json()
 
-cloud = shade.OpenStackCloud()
-token = cloud.auth_token
-placement_svc = cloud.get_service('placement')
-placement_endpoint = cloud.search_endpoints(
-    filters=dict(service_id=placement_svc.id, interface='public'))[0]
 
-api_url = placement_endpoint['url']
-print('using placement api @ {}'.format(api_url))
+def parse_args():
+    p = argparse.ArgumentParser()
 
-tally = {}
-providers = {}
-placement = Placement(api_url, cloud.keystone_session)
-for provider in placement.list_resource_providers():
-    providers[provider['uuid']] = provider
-    allocations = placement.get_resource_provider_allocations(provider['uuid'])
-    for instance_uuid, allocation in allocations['allocations'].items():
-        if instance_uuid not in tally:
-            tally[instance_uuid] = {}
+    p.add_argument('--fix', action='store_true')
+    p.add_argument('--dry-run', '-n', action='store_true')
 
-        tally[instance_uuid][provider['uuid']] = allocation
+    cloud_config.register_argparse_arguments(p, sys.argv)
 
-with open('dump.json', 'w') as fd:
-    json.dump({'providers': providers, 'tally': tally}, fd,
-              indent=2)
+    p.set_defaults(loglevel='INFO')
+    return p.parse_args()
 
-for instance_uuid, allocations in tally.items():
-    if len(allocations) > 1:
-        print('{} has multiple allocations'.format(instance_uuid))
-        instance = cloud.get_server(instance_uuid, all_projects=True)
-        if instance:
-            current_hypervisor = instance.get(
-                'OS-EXT-SRV-ATTR:hypervisor_hostname')
-        else:
-            current_hypervisor = None
 
-        for provider_uuid, allocation in allocations.items():
-            provider = providers[provider_uuid]
-            if provider['name'] == current_hypervisor:
-                mark = '*'
+def main():
+    args = parse_args()
+    logging.basicConfig(level=args.loglevel)
+
+    cloud = cloud_config.get_one_cloud(argparse=args)
+    cloud_api = shade.OpenStackCloud(cloud_config=cloud)
+    placement = Placement(cloud_api)
+
+    tally = {}
+    providers = {}
+
+    LOG.info('getting resource allocations')
+    for provider in placement.list_resource_providers():
+        providers[provider['uuid']] = provider
+        allocations = placement.get_resource_provider_allocations(provider['uuid'])
+        for instance_uuid, allocation in allocations['allocations'].items():
+            if instance_uuid not in tally:
+                tally[instance_uuid] = {}
+
+            tally[instance_uuid][provider['uuid']] = allocation
+
+    LOG.info('auditing allocations')
+    for instance_uuid, allocations in tally.items():
+        if len(allocations) > 1:
+            print('{} has multiple allocations'.format(instance_uuid))
+            instance = cloud_api.get_server(instance_uuid, all_projects=True)
+            if instance:
+                current_hypervisor = instance.get(
+                    'OS-EXT-SRV-ATTR:hypervisor_hostname')
             else:
-                mark = '-'
-            print('{} {} ({})'.format(mark,
-                                      provider['name'],
-                                      provider_uuid))
+                current_hypervisor = None
+
+            for provider_uuid, allocation in allocations.items():
+                provider = providers[provider_uuid]
+                if provider['name'] == current_hypervisor:
+                    mark = '*'
+                else:
+                    mark = '-'
+                print('{} {} ({})'.format(mark,
+                                          provider['name'],
+                                          provider_uuid))
+
+
+if __name__ == '__main__':
+    main()
